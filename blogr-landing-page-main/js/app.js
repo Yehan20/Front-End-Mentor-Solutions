4583ac297d7350557242f0937e55e15bc7c489ba
Yehan NK <yehansk15@gmail.com>
2022-03-26 17:58:37 +0530

    nav bar done

--- blogr-landing-page-main/js/app.js
+++ blogr-landing-page-main/js/app.js
@@ -4,19 +4,67 @@ function toggleNavBar(){
     const list = document.querySelector('.list');
     const image= document.querySelector('.image__icon')
     toggler.addEventListener('click',()=>{
-        if(list.style.display===`none`){
-            list.style.display=`flex`;
+
+        if(list.style.display===`none`||list.style.display===``){
             image.src='images/icon-close.svg'
+            list.style.display=`flex`;
+            console.log('clicked');
+           
            
          
         }
         else {
+            image.src='images/icon-hamburger.svg'
             list.style.display=`none`;
 
-            image.src='images/icon-hamburger.svg'
-        };
+     
+        }
         
     })
 
 }
 toggleNavBar();
+
+// sub list toggler
+function toggleSublist(){
+
+    const subList=document.querySelector('.sub__list');
+    document.querySelector('.sub').addEventListener('click',(e)=>{
+        e.preventDefault();
+          if(subList.style.display===`none`||subList.style.display===``){
+              subList.style.display=  `block`;
+          }
+          else{
+              subList.style.display=  `done`;
+          }
+    })
+
+}
+//toggleSublist();
+if(window.innerWidth<799)addDarkArrows();
+else addLightArrows();
+
+window.addEventListener('resize',(e)=>{
+   
+    if(window.innerWidth<799){
+        console.log('small')
+        addDarkArrows();
+       
+    }
+    else{
+        addLightArrows();
+    }
+})
+
+function addDarkArrows(){
+    const arrow = document.querySelectorAll('#arrow');
+    arrow.forEach((link)=>{
+        link.src='images/icon-arrow-dark.svg'
+    })
+}
+function addLightArrows(){
+    const arrow = document.querySelectorAll('#arrow');
+    arrow.forEach((link)=>{
+        link.src='images/icon-arrow-light.svg'
+    })
+}
