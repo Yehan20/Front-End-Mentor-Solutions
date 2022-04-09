2681aa5a249618d6854967b04d771f99b5ec9535
Yehan NK <yehansk15@gmail.com>
2022-04-09 15:25:55 +0530

    .

--- time-tracking-dashboard-main/js/app.js
+++ time-tracking-dashboard-main/js/app.js
@@ -1,7 +1,5 @@
 // Click links change color
 const links = document.querySelectorAll(".time");
-const container = document.querySelector(".container");
-const header = document.querySelector(".user__profile");
 
 // activities
 const work = document.querySelector('#work .time');
@@ -11,21 +9,15 @@ const social = document.querySelector('#social .time');
 const selfcare = document.querySelector('#selfcare .time');
 const exercise = document.querySelector('#exercise .time');
 
-//row
-//sconst rowTime = document.querySelectorAll('.row.time');
-// console.log(rowTime);
-
-
+const library = new Library(work,play,study,exercise,social,selfcare);
 
 links.forEach((link)=>{
     link.addEventListener('click',(e)=>{
+        
         e.preventDefault();
         removeLinks(links);
         link.classList.add("active");
-        fetchData(link.id)
-
-  
-        
+        library.fetchData(link.id);       
     })
  
 })
@@ -40,135 +32,3 @@ function removeLinks(links){
     })
 }
 
-function fetchData(day){
-    fetch("../data.json").then(res=>res.json()).then((data)=>{
-        let datas;
-        data.forEach((titles)=>{
-     
-            activityType=((Object.entries(titles)[0][1]))
-          if(day==="daily"){
-          
-          
-            datas=((Object.entries(titles.timeframes.daily)))
-            console.log(datas[0][1],datas[1][1])
-            // rowTime.forEach(((row)=>{
-            //     row.innerHTML
-            // }))
-            switch(activityType){
-                case "Work":
-                work.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Play":
-                play.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Study":
-                study.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Exercise":
-                exercise.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Social":
-                social.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-                case "Self Care":
-                selfcare.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-            }
-          }
-         
-          
-          if(day==="weekly"){
-             
-            datas=((Object.entries(titles.timeframes.weekly)))
-          
-            switch(activityType){
-                case "Work":
-                work.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Play":
-                play.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Study":
-                study.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Exercise":
-                exercise.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Social":
-                social.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-                case "Self Care":
-                selfcare.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-            }
-              
-          }
-           
-
-          if(day==="monthly"){
-
-            datas=((Object.entries(titles.timeframes.monthly)))
-          
-            switch(activityType){
-                case "Work":
-                work.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Play":
-                play.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Study":
-                study.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Exercise":
-                exercise.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break;
-                
-                case "Social":
-                social.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-                case "Self Care":
-                selfcare.innerHTML=` <h3>${datas[0][1]}hrs</h3>
-                <p>Last week ${datas[0][1]}-hrs</p> `    
-                break; 
-
-            }
-           }
-          
-        })
-        // console.log(datas)
-       
-    })
-}
---
+++ time-tracking-dashboard-main/js/library.js
@@ -0,0 +1,84 @@
+class Library{
+    
+    constructor(work,play,study,exercise,social,selfcare){
+        this.work =work;
+        this.play =play;
+        this.study =study;
+        this.exercise =exercise;
+        this.social =social;
+        this.selfcare =selfcare;
+
+    }
+
+
+    fetchData(day){
+        fetch("../data.json").then(res=>res.json()).then((data)=>{
+            let datas;
+            let activityType;
+            data.forEach((titles)=>{
+                activityType=((Object.entries(titles)[0][1])) // get the activities each loop different activity
+
+              if(day==="daily"){ 
+              
+                datas=((Object.entries(titles.timeframes.daily))) // get the data per each time each loop new set added
+
+                this.addData(datas,activityType); // each loop this method is called , new set of data is added depending on activity type
+              }
+             
+              
+              if(day==="weekly"){
+                 
+                datas=((Object.entries(titles.timeframes.weekly)))    
+                this.addData(datas,activityType);
+                  
+              }
+               
+    
+              if(day==="monthly"){
+    
+                datas=((Object.entries(titles.timeframes.monthly)))     
+                this.addData(datas,activityType);
+               }
+              
+            })
+           
+        })
+    }
+
+
+    addData(data,type){
+     
+      switch(type){
+          case "Work":
+          work.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break;
+          
+          case "Play":
+          play.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break;
+          
+          case "Study":
+          study.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break;
+          
+          case "Exercise":
+          exercise.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break;
+          
+          case "Social":
+          social.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break; 
+  
+          case "Self Care":
+          selfcare.innerHTML=` <h3>${data[0][1]}hrs</h3>
+          <p>Last week ${data[0][1]}-hrs</p> `    
+          break; 
+  
+       }
+   }   
+}
