0ba19c8cec2e11c39dd57357d233b96bf6fb8f57
Yehan NK <yehansk15@gmail.com>
2022-05-14 14:04:18 +0530

    .

--- intro-section-with-dropdown-navigation-main/js/app.js
+++ intro-section-with-dropdown-navigation-main/js/app.js
@@ -1,6 +1,9 @@
 // Nav bar Toggle Effect
 const nav = document.querySelector('.nav__bar');
 
+
+//  Mobile view toggle functionality 
+
 document.querySelector('#toggleBtn').addEventListener("click",()=>{
     const list  =document.querySelector('.list');
     let cross = document.querySelector('.cross');
@@ -20,31 +23,66 @@ document.querySelector('#toggleBtn').addEventListener("click",()=>{
     }
 })
 
+//  Arrow changing funcionality mobile view
+const dropDownFunction =(e)=>{
+    e.preventDefault();
+
+    let arrow=(e.target.lastElementChild);
+    let sublist=e.target.nextElementSibling;
+    if(arrow){
+       if(!arrow.classList.contains("reverse")){
+           arrow.src=`images/icon-arrow-up.svg`
+           arrow.classList.add("reverse")
+           sublist.classList.toggle("remove02");
+       }
+       else{
+           arrow.src=`images/icon-arrow-down.svg`
+           arrow.classList.remove("reverse")
+              sublist.classList.toggle("remove02")
+       }
+    }
+
+    if(e.target.className==='dropdown__arrow'){
+       e.target.src=`images/icon-arrow-up.svg`
+    }
+    
+}
+
 
 //  Drop Down List Affect
 const dropDown  = document.querySelectorAll("#dropDown");
 dropDown.forEach((dropDown)=>{
-     dropDown.addEventListener('click',(e)=>{
-         e.preventDefault();
+     dropDown.addEventListener('click',dropDownFunction)
+})
 
-         let arrow=(e.target.lastElementChild);
-         let sublist=e.target.nextElementSibling;
-         if(arrow){
-            if(!arrow.classList.contains("reverse")){
-                arrow.src=`images/icon-arrow-up.svg`
-                arrow.classList.add("reverse")
-                sublist.style.display=`block`;
-            }
-            else{
-                arrow.src=`images/icon-arrow-down.svg`
-                arrow.classList.remove("reverse")
-                sublist.style.display=`none`;
-            }
-         }
+// if user uses a tablet 
+if(innerWidth>767){
 
-         if(e.target.className==='dropdown__arrow'){
-            e.target.src=`images/icon-arrow-up.svg`
-         }
-         
+    dropDown.forEach((dropDown)=>{
+        dropDown.removeEventListener('click',dropDownFunction)
      })
+      console.log("ok")
+   }
+
+
+
+  // remove the click effect in larger screen 
+window.addEventListener("resize",(e)=>{
+   if(innerWidth>767){
+    dropDown.forEach((dropDown)=>{
+        dropDown.removeEventListener('click',dropDownFunction)
+     })
+
+     let sub = document.querySelectorAll('.sub__list');
+     sub.forEach((su)=>{su.classList.add('remove01') 
+                       su.classList.remove('remove02')})
+   
+      console.log("ok")
+   }
+   else{
+    dropDown.forEach((dropDown)=>{
+        dropDown.addEventListener('click',dropDownFunction)
+   })
+   }
+ 
 })
