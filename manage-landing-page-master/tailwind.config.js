844ff25da620f3e711f376c12c464a1d9ed6ad2a
Yehan NK <yehansk15@gmail.com>
2022-09-03 12:25:26 +0530

    done some html

--- manage-landing-page-master/tailwind.config.js
+++ manage-landing-page-master/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container:{
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1140px'
+        
+      },
+    },
+    extend: {
+      colors:{
+        'DarkBlue-clr':'hsl(228, 39%, 23%)'
+      }
+    },
   },
   plugins: [],
 }
